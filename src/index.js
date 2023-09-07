8ed7e43dcad5f104806a94e41904d713a25b69a4
jbalaraman <james.balaraman@gmail.com>
2023-09-07 12:02:04 -0600

    Added some comments

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 document.addEventListener('DOMContentLoaded', () => {
+    //Some of the DOM Elements' definitions
     const holidayElement = document.getElementById('holiday');
     const dateElement = document.getElementById('date');
     const buntingStatusElement = document.getElementById('buntingStatus');
@@ -10,6 +11,7 @@ document.addEventListener('DOMContentLoaded', () => {
     let holidays;
     let holidayCounter = 0;
 
+    //Updates the holiday info
     function updateHolidayInfo(holidayIndex) {
         const holiday = holidays[holidayIndex];
         holidayElement.textContent = `Holiday: ${holiday.title}`;
@@ -18,10 +20,12 @@ document.addEventListener('DOMContentLoaded', () => {
         notesElement.textContent = `Notes: ${holiday.notes}`;
     }
 
+    //Calls the function above, uses the holidayCounter variable
     function updateCurrentHoliday() {
         updateHolidayInfo(holidayCounter);
     }
 
+    //Forward and back buttons code for updating the holiday card
     const nextButton = document.getElementById('nextHoliday');
     nextButton.addEventListener('click', () => {
         holidayCounter++;
@@ -40,6 +44,7 @@ document.addEventListener('DOMContentLoaded', () => {
         updateCurrentHoliday();
     });
 
+    //functionality for the comment section feature, code for the submit button
     commentForm.addEventListener('submit', (e) => {
         e.preventDefault();
         const commentText = commentInput.value.trim();
@@ -57,12 +62,14 @@ document.addEventListener('DOMContentLoaded', () => {
         }
     });
 
+    //grabs the data from the API
     fetch(`https://www.gov.uk/bank-holidays.json`)
     .then(resp => resp.json())
     .then(data => {
         holidays = data['england-and-wales'].events;
         updateCurrentHoliday();
 
+        //displays list of the holidays below the comment section
         const allHolidaysList = document.getElementById('allHolidaysList');
         holidays.forEach((holiday, index) => {
             const listItem = document.createElement('li');
